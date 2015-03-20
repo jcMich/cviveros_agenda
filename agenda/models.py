95c23762136b9dbb518900179fa150d45eb71adf
Cviveros <christianviverost@gmail.com>
2015-03-20 14:30:15 -0600

    hola commit

--- agenda/models.py
+++ agenda/models.py
@@ -17,7 +17,7 @@ class Contacto(models.Model):
     owner = models.ForeignKey(User)
     nombre = models.CharField(max_length=150)
     telefono = models.CharField(max_length=20, verbose_name='tel')
-    grupo = models.ForeignKey(Grupo)
+    grupo = models.ForeignKey(Grupo, null=True, blank=True)
 
     def __str__(self):
         return self.nombre
